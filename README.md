328b3ee95d1b264e8c32f24d60397514fafd290f
dustytrash <Lionelbergen@live.com>
2025-02-16 15:41:18 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Generates a random phrase and automatically adds it to the users keyboard
+-------------------------------------------------------------------------   
+
+## Quick Start  
+1) Add phrases/texct to `text.txt` file  
+2) Run the file.   
+3) Optionally, set a keybind that automatically runs the batch file.   
+
+
---
+++ random_phrase.bat
@@ -0,0 +1,30 @@
+@ECHO OFF
+SETLOCAL ENABLEDELAYEDEXPANSION
+SET file=text.txt
+SET /a lineCount=0
+SET /a lines
+SET /a randomNumber=-1
+
+for /F "tokens=*" %%A IN (%file%) DO (
+	SET /a lineCount=lineCount+1
+	SET lines[!lineCount!]=%%A
+)
+
+CALL :randomNumber 1 %lineCount% randomNumber
+ECHO Random Number Generated: %randomNumber%
+ECHO %file% has %lineCount% lines.
+
+ECHO !lines[%randomNumber%]!
+
+REM Set the line of test to the clipboard
+ECHO|set /p=!lines[%randomNumber%]!|clip
+
+GOTO:EOF
+
+:randomNumber
+	SET /a minValue = %~1
+	SET /a maxValue = %~2
+
+	REM SET /A %~3=!RANDOM! * (%maxValue% - %minValue% + 1) / 32768 + %minValue%
+	SET /A %~3=(!random! %%!maxValue!) + %minValue%
+GOTO:EOF
